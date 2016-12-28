1f45ca46a988978232634b3df1a8763a206c0008
YiLung <skek6643@skl.com.tw>
2016-12-28 20:14:06 +0800

    first commit

---
+++ Snake/Snake.swift
@@ -0,0 +1,13 @@
+//
+//  Snake.swift
+//  Snake
+//
+//  Created by lung on 2016/12/28.
+//  Copyright © 2016年 lung. All rights reserved.
+//
+
+import UIKit
+
+class Snake: NSObject {
+
+}
---
+++ SnakeView.swift
@@ -0,0 +1,21 @@
+//
+//  SnakeView.swift
+//  Snake
+//
+//  Created by lung on 2016/12/28.
+//  Copyright © 2016年 lung. All rights reserved.
+//
+
+import UIKit
+
+class SnakeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
