227eabba242e636aa5a4e53cc3a4c4b6e8af13c9
YiLung <skek6643@skl.com.tw>
2016-12-28 16:50:19 +0800

    Initial Commit

---
+++ Snake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snake
+//
+//  Created by lung on 2016/12/28.
+//  Copyright © 2016年 lung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
